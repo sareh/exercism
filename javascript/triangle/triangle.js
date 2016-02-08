2447333fde7c0925cfd85ad0dc342c0a486ca89a
sareh <sareh.heidari@gmail.com>
2016-02-08 22:03:14 +0000

    JavaScript Triangle solution

---
+++ javascript/triangle/triangle.js
@@ -0,0 +1,12 @@
+function Triangle(a, b, c) {
+  this.kind = function() {
+    if(a <= 0 || b <= 0 || c <= 0 ) throw new Error('Invalid input - can\'t have non-positive sides.'); 
+    var sorted = [a,b,c].sort();
+    if(sorted[0]+sorted[1] < sorted[2]) throw new Error('Illegal input - violates triangle inequality.');
+    if(a === b && b === c) return 'equilateral'; 
+    if(a === b || b === c || a === c) return 'isosceles';
+    return 'scalene';
+  }
+}
+
+module.exports = Triangle;
