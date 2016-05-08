242d9cc7a2743411ed1496db11a53ec532eb4ac5
sareh <sareh.heidari@gmail.com>
2016-05-08 21:30:24 +0100

    Passes first two tests

---
+++ javascript/clock/clock.js
@@ -0,0 +1,12 @@
+var Clock = function () {
+}
+Clock.prototype.at = function (hours, mins) {
+  this.time = ':';
+  hours = (hours < 10) ? '0' + hours : hours;
+  mins = (mins < 10) ? '0' + mins : mins;
+  this.time = hours + this.time;
+  this.time = this.time + (mins || '00');
+  return this.time;
+}
+
+module.exports = {"at": Clock.prototype.at};
