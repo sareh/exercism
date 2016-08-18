9f6e26134d923f8adfbbcf8721c83da5b728b1bc
sareh <sareh.heidari@gmail.com>
2016-08-18 07:58:15 +0100

    Comment

--- ecmascript/hamming/babel-output/hamming.js
+++ ecmascript/hamming/babel-output/hamming.js
@@ -1,12 +1,13 @@
-"use strict";
+// The most readable version, using a for loop
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 var Hamming = (function () {
   function Hamming() {
@@ -14,14 +15,26 @@ var Hamming = (function () {
   }
 
   _createClass(Hamming, [{
-    key: "compute",
-    value: function compute(a, b) {
-      return 0;
+    key: 'compute',
+    value: function compute(strand1, strand2) {
+      var count = 0;
+
+      if (strand1.length !== strand2.length) {
+        throw new Error('DNA strands must be of equal length.');
+      }
+
+      for (var i = 0; i < strand1.length; i++) {
+        if (strand1[i] !== strand2[i]) {
+          count += 1;
+        }
+      }
+
+      return count;
     }
   }]);
 
   return Hamming;
 })();
 
-exports["default"] = Hamming;
-module.exports = exports["default"];
+exports['default'] = Hamming;
+module.exports = exports['default'];
--- ecmascript/hamming/hamming.js
+++ ecmascript/hamming/hamming.js
@@ -1,5 +1,18 @@
+// The most readable version, using a for loop
 export default class Hamming {
-  compute(a, b) {
-    return 0
+  compute(strand1, strand2) {
+    let count = 0;
+
+    if (strand1.length !== strand2.length) {
+      throw new Error('DNA strands must be of equal length.')
+    }
+
+    for(let i=0; i < strand1.length; i++) {
+      if(strand1[i] !== strand2[i]) {
+        count += 1
+      }
+    }
+    
+    return count
   }
 }
